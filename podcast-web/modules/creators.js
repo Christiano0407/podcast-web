13b6083834343ca7377340278f6da16189373288
Christiano0407 <placebo0704@gmail.com>
2023-04-27 12:35:20 -0600

    new carousel and images

--- podcast-web/modules/creators.js
+++ podcast-web/modules/creators.js
@@ -1,6 +1,11 @@
 //** === ========== Creators ============ ===  */
 //**? === ==== Import ==== === */
 import sliderImg from '../src/img/imgpodNew1.jpg';
+import sliderImgOne from '../src/img/imgPodTwo.jpg';
+import sliderImgTwo from '../src/img/imgPodThree.jpg';
+import sliderImgThree from '../src/img/imgPodSix.jpg';
+import sliderPodFour from '../src/img/imgPodFive.jpg';
+import sliderPodFive from '../src/img/podImage1.jpg';
 //**! === Variables === */
 const slider = document.querySelector(`#slider`);
 const containerSlider = document.querySelector(`#sliderContainer`);
@@ -10,13 +15,55 @@ let currentPosition = 0;
 
 //**! === ==== Slider Carousel ==== === */
 slider.innerHTML = `
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
-  <img class="imgSlider" id="idImg"  src=${sliderImg}>
+  <div class="carousel">
+    <img class="imgSlider" id="idImg"  src=${sliderImgTwo}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+    <img class="imgSlider" id="idImg"  src=${sliderImgThree}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+    <img class="imgSlider" id="idImg"  src=${sliderImgOne}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+    <img class="imgSlider" id="idImg"  src=${sliderPodFour}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+     <img class="imgSlider" id="idImg"  src=${sliderPodFive}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+    img class="imgSlider" id="idImg"  src=${sliderImgOne}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
+  <div class="carousel">
+    <img class="imgSlider" id="idImg"  src=${sliderImgTwo}>
+    <div class="carousel-display">
+        <button class="display-icon"><i class="fa-solid fa-play"></i></button>
+        <span>How to create your voice in a Podcast</span>
+    </div>
+  </div>
 `;
 //**! === Create a JavaScript function that will handle the slider scrolling ===  */
 const scrollSlider = (amount) => {
